8d3712e92a1668eccd4a1d5486a28c1fdda4d83b
BekiaD <144695091+BekiaD@users.noreply.github.com>
2024-03-13 19:53:48 +0100

    folder reorganization

---
+++ Digital Businesscard Solo Project copy/src/Interests.jsx
@@ -0,0 +1,14 @@
+import './Interests.css'
+
+export default function Interests() {
+    return (
+      <div className="interests">
+        <h2 className="sub-title">Interests</h2>
+        <p className="text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
+          corrupti dolore quasi incidunt doloribus dolorum ut aliquid qui.
+        </p>
+      </div>
+    );
+  }
+  
