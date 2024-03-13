d0fa9733e6459cb22925d604b38aa3c4527b0645
BekiaD <144695091+BekiaD@users.noreply.github.com>
2024-03-13 16:18:24 +0100

    final
    refactored too CSS for components

---
+++ Digital Businesscard Solo Project/src/About.jsx
@@ -0,0 +1,14 @@
+import './About.css'
+
+export default function About() {
+    return (
+      <div className="about">
+        <h2 className="sub-title">About</h2>
+        <p className="text">
+          I love to code and play with HTML, CSS, and JavaScript. I am currently
+          learning React. I am also learning Node.
+        </p>
+      </div>
+    );
+  }
+  
